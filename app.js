6cac83e2cdfa189adc8672b29ab0cb7a9b026cdb
pimenvibritania <pimenvibritania@pimenvibritanias-MacBook-Pro.local>
2020-10-04 17:45:45 +0700

    Add PM2 as Production Process Manager

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var personsRouter = require('./routes/persons');
+
 
 var app = express();
 
@@ -16,5 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/persons', personsRouter);
 
 module.exports = app;
